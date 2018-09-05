16551c483c3b9c0a2c184148d26b28c8e51ec564
Irving Angulo <irvang.manage@gmail.com>
2018-09-04 22:49:54 -0400

    Removing space on phone.

--- src/components/OrganizationCard.js
+++ src/components/OrganizationCard.js
@@ -12,7 +12,7 @@ class OrganizationCard extends Component {
         <p className="lead">{category}</p>
         <p>{overview}</p>
         <p><span className="fa fa-map-o"></span> {location}</p>
-        {phone && <p> {phone}</p>}
+        {phone && <p>{phone}</p>}
         {website ? <p><span className="fa fa-link"></span> <a href={website}>Website</a></p> : <div />}
         <ul className="list-inline">
           {facebookUrl && <li><a href="{facebookUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
