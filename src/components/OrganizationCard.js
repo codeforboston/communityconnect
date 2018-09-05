eb262c8fccebc1311ff33ceac07371a8da11aa18
Irving Angulo <irvang.manage@gmail.com>
2018-09-04 21:05:59 -0400

    Using destructuring assignments to abbreviate code. (#25)
    * Using destructuring assignments to abbreviate code.
    
    * Destructuring for clarity.
    
    * 2 spaces
    
    * space > tabs

--- src/components/OrganizationCard.js
+++ src/components/OrganizationCard.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import { Card } from 'reactstrap';
 
 class OrganizationCard extends Component {
-    render() {
-        return (
-            <Card>
-                <h3>{this.props.organization.name}</h3>
-                <p className="lead">{this.props.organization.category}</p>
-                <p>{this.props.organization.overview}</p>
-                <p><span className="fa fa-map-o"></span> {this.props.organization.location}</p>
-                {this.props.organization.website? <p><span className="fa fa-link"></span> <a href={this.props.organization.website}>Website</a></p>: <div/>}
-                <ul className="list-inline">
-                    {this.props.organization.facebookUrl && <li><a href="{this.props.organization.facebookUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
-                    {this.props.organization.instagramUrl && <li><a href="{this.props.organization.instagramUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
-                    {this.props.organization.twitterUrl && <li><a href="{this.props.organization.twitterUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
-                </ul>
-            </Card>
-        );
-    }
+  render() {
+    const { name, category, overview, location, website, facebookUrl,
+      instagramUrl, twitterUrl } = this.props.organization;
+
+    return (
+      <Card>
+        <h3>{name}</h3>
+        <p className="lead">{category}</p>
+        <p>{overview}</p>
+        <p><span className="fa fa-map-o"></span> {location}</p>
+        {website ? <p><span className="fa fa-link"></span> <a href={website}>Website</a></p> : <div />}
+        <ul className="list-inline">
+          {facebookUrl && <li><a href="{facebookUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
+          {instagramUrl && <li><a href="{instagramUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
+          {twitterUrl && <li><a href="{twitterUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
+        </ul>
+      </Card>
+    );
+  }
 }
 
 export default OrganizationCard;
