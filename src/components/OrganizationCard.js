6d26a22cdc32fdcd94cdbd74b9811f603522b599
Irving Angulo <irvang.manage@gmail.com>
2018-09-08 10:13:45 -0400

    Added social urls variables, they were not there. Social's ul will now only display if either of the social url's is present. Did so to avoid an empty space on Card. Changed 'overview' to use conditional in order to avoid empty paragraph being displayed.

--- src/components/OrganizationCard.js
+++ src/components/OrganizationCard.js
@@ -3,22 +3,22 @@ import { Card } from 'reactstrap';
 
 class OrganizationCard extends Component {
   render() {
-    const { name, category, overview, location, website, facebookUrl,
+    let { name, category, overview, location, website, facebookUrl,
       instagramUrl, twitterUrl, phone } = this.props.organization;
 
     return (
       <Card>
         <h3>{name}</h3>
         <p className="lead">{category}</p>
-        <p>{overview}</p>
+        {overview && <p>{overview}</p>}
         <p><span className="fa fa-map-o"></span> {location}</p>
         {phone && <p>{phone}</p>}
         {website ? <p><span className="fa fa-link"></span> <a href={website}>Website</a></p> : <div />}
-        <ul className="list-inline">
-          {facebookUrl && <li><a href="{facebookUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
-          {instagramUrl && <li><a href="{instagramUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
-          {twitterUrl && <li><a href="{twitterUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
-        </ul>
+        {(facebookUrl || instagramUrl || twitterUrl) && <ul className="list-inline">
+          {facebookUrl && <li><a href="{facebookUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square">{facebookUrl}</i></a></li>}
+          {instagramUrl && <li><a href="{instagramUrl}"data-type="social"><i className="fa fa-2x fa-facebook-square">{instagramUrl}</i></a></li>}
+          {twitterUrl && <li><a href="{twitterUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square">{twitterUrl}</i></a></li>}
+        </ul>}
       </Card>
     );
   }
