73dd3254c4d7ceb1e3363368a36a3846c48cdaae
Irving Angulo <irvang.manage@gmail.com>
2018-09-11 11:53:43 -0400

    Irv display phone (#26)
    * Added phone to OrganizationCard.

--- src/components/OrganizationCard.js
+++ src/components/OrganizationCard.js
@@ -4,7 +4,7 @@ import { Card } from 'reactstrap';
 class OrganizationCard extends Component {
   render() {
     const { name, category, overview, location, website, facebookUrl,
-      instagramUrl, twitterUrl } = this.props.organization;
+      instagramUrl, twitterUrl, phone } = this.props.organization;
 
     return (
       <Card>
@@ -12,6 +12,7 @@ class OrganizationCard extends Component {
         <p className="lead">{category}</p>
         <p>{overview}</p>
         <p><span className="fa fa-map-o"></span> {location}</p>
+        {phone && <p>{phone}</p>}
         {website ? <p><span className="fa fa-link"></span> <a href={website}>Website</a></p> : <div />}
         <ul className="list-inline">
           {facebookUrl && <li><a href="{facebookUrl}" data-type="social"><i className="fa fa-2x fa-facebook-square"></i></a></li>}
