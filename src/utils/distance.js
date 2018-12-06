d2799f575cd1950475994cf6dccb8233d586f153
Theodore Shin <tdyshin@gmail.com>
2018-12-06 18:24:47 -0500

    Fix bug with distance showing, clean up code, and add comments

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -1,30 +1,36 @@
 
-export const getDistance = (a, b) => {
-  if(!a.coordinates || !b.coordinates){
+export const getDistance = (targetLocation, myLocation) => {
+  // return undefined if we are missing either coordinate
+  if(!targetLocation.coordinates || !myLocation.coordinates) {
     return;
   }
 
-  let radians = (degrees) => {
-    return degrees * Math.PI / 180;
-  };
-  
-  //const d2r = Math.PI / 180;
-  var latA = radians(a.coordinates.lat);
+  // haversine formula is used
+  // https://www.movable-type.co.uk/scripts/latlong.html
+  const DEGREES_TO_RADIANS = degrees => degrees * Math.PI / 180;
+  const RADIUS_OF_EARTH = 6371000;
 
+  // convert degrees to radians
+  let latTargetLocation = DEGREES_TO_RADIANS(targetLocation.coordinates.lat);
+  let latMyLocation = DEGREES_TO_RADIANS(myLocation.coordinates.lat);
 
-  var latB = radians(b.coordinates.lat);
+  // calculate changes in latitude and longitude
+  let changeInLat  = DEGREES_TO_RADIANS(myLocation.coordinates.lat - targetLocation.coordinates.lat);
+  let changeInLong =  DEGREES_TO_RADIANS(myLocation.coordinates.lng - targetLocation.coordinates.lng);
 
+  // a is the square of half the chord length between the points
+  let a = Math.pow(Math.sin(changeInLat/2), 2) + Math.cos(latTargetLocation) * Math.cos(latMyLocation) * Math.pow(Math.sin(changeInLong/2), 2);
 
-  var dlong  = radians(a.coordinates.lat - b.coordinates.lat);
-  var dlat =  radians(b.coordinates.lng - b.coordinates.lng);
+  // c is angular distance in radians
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  var d = Math.pow(Math.sin( dlong/2.0), 2) + Math.cos(latA) * Math.cos(latB) * Math.pow(Math.sin(dlat/2.0), 2);
+  let distanceInMeters = RADIUS_OF_EARTH * c;
 
-  var c = 2 * Math.atan2(Math.sqrt(d), Math.sqrt(1-d));
+  const NUM_OF_METERS_IN_ONE_MILE = 1609.34;
+  const METERS_TO_MILES = meters => meters / NUM_OF_METERS_IN_ONE_MILE;
 
-  c = c * 6371000
+  // convert meters to miles and set to two decimal points
+  let distanceInMiles = +(METERS_TO_MILES(distanceInMeters)).toFixed(2);
 
-  return (.000621371 * c);
-
-
+  return distanceInMiles;
 }
