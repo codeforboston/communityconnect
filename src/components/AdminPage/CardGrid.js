b095716d5e4ed76cd73ce99639c6eeb7b560289f
Alex Hughes <alxhghs@gmail.com>
2019-02-28 15:25:26 -0500

    finish merging master

--- src/components/AdminPage/CardGrid.js
+++ src/components/AdminPage/CardGrid.js
@@ -1,96 +1,100 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { Row, Col, Container } from 'reactstrap';
+import { connect } from 'react-redux';
 import OrganizationCard from '../Common/OrganizationCard';
 import SortBar from '../Common/SortBar.js';
 import SearchBar from '../Header/SearchBar';
-import {getDistance} from '../../utils/distance.js';
+import { getDistance } from '../../utils/distance.js';
 
 export class CardGrid extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props){
+    super(props)
 
-        this.state = {
-            dataSort: this.sortByAlphabet,
-        }
+    this.state = {
+      dataSort: this.sortByAlphabet,
+    }
+  }
+
+  getCloserResource = (a , b) => {
+    if(getDistance(a,this.props.currentPos)
+    > getDistance(b,this.props.currentPos)){
+      return 1;
     }
 
-    getCloserResource = (a, b) => {
-        if (getDistance(a, this.props.currentPos)
-            > getDistance(b, this.props.currentPos)) {
-            return 1;
-        }
+    return -1;
+  }
 
-        return -1;
-    }
-
-    getCloserName = (a, b) => {
-        if (a.name > b.name) return 1
-        else if (a.name < b.name) return -1
-        else return 0
-    }
+  getCloserName = (a, b) => {
+    if(a.name > b.name) return 1
+    else if(a.name < b.name ) return -1
+    else return 0
+  }
 
 
-    sortByAlphabet = () => {
-        return this.props.resource.slice().sort(this.getCloserName);
-    }
+  sortByAlphabet = () => {
+    return this.props.resource.slice().sort(this.getCloserName);
+  }
 
-    sortByDistance = () => {
-        return this.props.resource.slice().sort(this.getCloserResource);
-    }
+  sortByDistance = () => {
+    return this.props.resource.slice().sort(this.getCloserResource);
+  }
 
-    handleSortChange = (newSort) => {
-        if (this.state.dataSort !== newSort)
-            this.setState({
-                // Set the dataSort variable to whichever sort function is chosen
-                dataSort: newSort,
-            })
-    }
+  handleSortChange = (newSort) => {
+    if(this.state.dataSort !== newSort)
+      this.setState({
+        // Set the dataSort variable to whichever sort function is chosen
+        dataSort: newSort,
+      })
+  }
 
-    render() {
-        const sortOptions = [
-            {key: 'Alphabetically', sort: this.sortByAlphabet, disabled: false}
-            , {key: 'Distance', sort: this.sortByDistance, disabled: !this.props.currentPos}
-        ];
+  render() {
+    const sortOptions = [
+      {key: 'Alphabetically', sort: this.sortByAlphabet, disabled: false}
+      ,{key: 'Distance', sort: this.sortByDistance, disabled: !this.props.currentPos}
+    ];
 
-        // Render will be called every time this.props.data is updated, and every time handleSortChange
-        // updates the this.state.dataSort variable.
-        // this.state.dataSort() sorts data to feed into the OrganizationCards without modifying the
-        // source of data
-        const sortedData = this.state.dataSort();
+    // Render will be called every time this.props.data is updated, and every time handleSortChange
+    // updates the this.state.dataSort variable.
+    // this.state.dataSort() sorts data to feed into the OrganizationCards without modifying the
+    // source of data
+    const sortedData = this.state.dataSort();
 
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-6">
-                        <SearchBar
-                            type="text"
-                            handleFilter={this.props.handleFilter}/>
-                    </div>
-                    <div className="col-sm-6" align="right"><SortBar
-                        onSortChange={this.handleSortChange}
-                        sortOptions={sortOptions}
-                    />
-                    </div>
-                </div>
-                <div className="card-deck">
-                    {
-                        sortedData.map((resource, index) => (
-                            <div className="col-sm-6 col" key={resource.id}>
-                                <OrganizationCard
-                                    key={resource.id}
-                                    index={resource.id}
-                                    organization={resource}
-                                    currentPos={this.props.currentPos}
-                                    saveItem={() => this.props.saveItem(resource)}
-                                    saveable={true}
-                                />
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
-        );
-    }
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <SearchBar
+              type="text"
+              handleFilter={this.props.handleFilter}
+            />
+          </Col>
+          <Col>
+            <SortBar
+              onSortChange={this.handleSortChange}
+              sortOptions={sortOptions}
+            />
+          </Col>
+        </Row>
+        <Row>
+          {
+            sortedData.map((resource, index) => (
+              // classes are bootstrap helper classes, see: https://getbootstrap.com/docs/4.0/utilities/flex/
+              <Col key={resource.id} xs="12" lg="6" className="d-flex my-2 align-content-stretch justify-content-stretch">
+                <OrganizationCard
+                  key={resource.id}
+                  index={resource.id}
+                  organization={resource}
+                  currentPos={this.props.currentPos}
+                  saveItem={() => this.props.saveItem(resource)}
+                  saveable={true}
+                />
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+    );
+  }
 };
 
 function mapStateToProps(state, ownProps) {
