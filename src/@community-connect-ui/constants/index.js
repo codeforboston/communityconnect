8ba695fe66fbca40215f99d8cf15cdda024ed72f
Alex Hughes <alxhghs@gmail.com>
2019-03-06 09:16:40 -0500

    css-in-jss: add radii

--- src/@community-connect-ui/constants/index.js
+++ src/@community-connect-ui/constants/index.js
@@ -2,6 +2,7 @@ import { breakPoints } from './breakPoints';
 import { colors } from "./colors";
 import { fonts } from './fonts'; 
 import { fontSizes } from './fontSizes';
+import { radii } from "./radii";
 import { spacing } from "./spacing";
 
 export {
@@ -9,5 +10,6 @@ export {
     colors,
     fonts,
     fontSizes,
+    radii,
     spacing
 }
--- src/components/Header/HeaderLayout.js
+++ src/components/Header/HeaderLayout.js
@@ -1,8 +1,8 @@
 import styled from "@emotion/styled";
-import { colors, spacing, fontSizes } from "../../@community-connect-ui/constants";
+import { colors, radii, spacing, fontSizes } from "../../@community-connect-ui/constants";
 
 export const Input = styled("input")`
-    border-radius: 4px;
+    border-radius: ${radii[0]};
     border: 1px solid ${colors.gray};
     padding-left: ${spacing[0]};
     font-size: ${fontSizes[1]};
