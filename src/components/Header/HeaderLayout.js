6a5842b4e41b97d1f78032d6c402ecf535e183c8
Alex Hughes <alxhghs@gmail.com>
2019-03-06 09:24:42 -0500

    css-in-jss: add relative imports for constants

--- src/components/Header/HeaderLayout.js
+++ src/components/Header/HeaderLayout.js
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled";
-import { colors, radii, spacing, fontSizes } from "../../@community-connect-ui/constants";
+import { colors, radii, spacing, fontSizes } from "@community-connect-ui/constants";
 
 export const Input = styled("input")`
     border-radius: ${radii[0]};
-    border: 1px solid ${colors.gray};
+    border: 1px solid ${colors.lightgray};
     padding-left: ${spacing[0]};
     font-size: ${fontSizes[1]};
 `;
