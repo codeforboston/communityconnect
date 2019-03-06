77fbdec42419a712cf41f56446f7afe270c65a99
Alex Hughes <alxhghs@gmail.com>
2019-03-06 09:14:13 -0500

    css-in-jss: add color constants

--- src/@community-connect-ui/constants/colors.js
+++ src/@community-connect-ui/constants/colors.js
@@ -1,7 +1,10 @@
 export const colors = {
-    lightGray: "",
-    gray: "gray",
-    white: "",
-    black: "",
-    turqoise: "",
-}
+    primary: "blue",
+    success: "green",
+    danger: "red",
+    black: "#000",
+    white: "#fff",
+    gray: "#212529",    
+    lightgray: "#00000080",
+    turqoise: "##007ea3",
+};
