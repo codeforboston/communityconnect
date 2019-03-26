dff2453587899d672a760620fad87ae9ac3fa405
Alex Hughes <alxhghs@gmail.com>
2019-03-26 18:29:38 -0400

    replace auto with 1fr for card grid

--- src/components/AdminPage/AdminPageLayout.js
+++ src/components/AdminPage/AdminPageLayout.js
@@ -28,12 +28,12 @@ export const CardListWrapper = styled("div")`
     gap: ${spacing[2]};
     padding: ${spacing[2]} 0;
     @media screen and (min-width: ${breakPoints[2]}) {
-        grid-template-columns: repeat(2, auto); 
+        grid-template-columns: repeat(2, 1fr); 
     }
     @media screen and (min-width: ${breakPoints[3]}) {
-        grid-template-columns: repeat(3, auto); 
+        grid-template-columns: repeat(3, 1fr); 
     }
     @media screen and (min-width: ${breakPoints[4]}) {
-        grid-template-columns: repeat(4, auto); 
+        grid-template-columns: repeat(4, 1fr); 
     }
 `;
