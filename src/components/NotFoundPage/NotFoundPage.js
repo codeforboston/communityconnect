27874bd4be1b1cf308adfc6e5aaaed1c77671eaf
Alex Hughes <alxhghs@gmail.com>
2019-03-27 12:17:22 -0400

    refactor not found page and remove inactive div wrapper

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Button } from 'reactstrap';
-
-import styles from './NotFoundPage.module.css';
+import { NotFoundPageLayout } from './NotFoundPageLayout';
 
 const NotFoundPage = ({currentPosition}) => {
         return (
-            <div className={styles.container}>
-                <div className="error-title">
-                    <h2>Error 404</h2>
-                    <h4>Page Not Found</h4>
-                    <p>
-                        The resource you are trying to access could not be found. 
-                        Navigate back to the Home Page and try again.
-                    </p>
-                    <Button tag={Link} to="/" color="primary">Go Home</Button>{' '}
-                </div>
-            </div>
+            <NotFoundPageLayout>
+                <h2>Error 404</h2>
+                <h4>Page Not Found</h4>
+                <p>
+                    The resource you are trying to access could not be found. 
+                    Navigate back to the Home Page and try again.
+                </p>
+                <Button tag={Link} to="/" color="primary">Go Home</Button>{' '}
+            </NotFoundPageLayout>
         );
 }
 
