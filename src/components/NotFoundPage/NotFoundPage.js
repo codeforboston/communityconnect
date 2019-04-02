799dc2e9ad2e34a60aa2201316606627e18a5857
Alex Hughes <alxhghs@gmail.com>
2019-04-02 16:39:22 -0400

    refactor header and not found page

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -4,17 +4,17 @@ import { Button } from 'reactstrap';
 import { NotFoundPageLayout } from './NotFoundPageLayout';
 
 const NotFoundPage = ({currentPosition}) => {
-        return (
-            <NotFoundPageLayout>
-                <h2>Error 404</h2>
-                <h4>Page Not Found</h4>
-                <p>
-                    The resource you are trying to access could not be found. 
-                    Navigate back to the Home Page and try again.
-                </p>
-                <Button tag={Link} to="/" color="primary">Go Home</Button>{' '}
-            </NotFoundPageLayout>
-        );
+    return (
+        <NotFoundPageLayout>
+            <h2>Error 404</h2>
+            <h4>Page Not Found</h4>
+            <p>
+                The resource you are trying to access could not be found. 
+                Navigate back to the Home Page and try again.
+            </p>
+            <Button tag={Link} to="/" color="primary">Go Home</Button>{' '}
+        </NotFoundPageLayout>
+    );
 }
 
 export default NotFoundPage;
