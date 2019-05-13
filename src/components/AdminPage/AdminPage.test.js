6e5ae095bc569d9845883498c28983a9eef34de0
Ravi <ravikancula@gmail.com>
2019-05-13 18:42:55 -0400

    Updated breaking snapshot

--- src/components/AdminPage/AdminPage.test.js
+++ src/components/AdminPage/AdminPage.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {shallow} from 'enzyme/build'
 import AdminPage from "./AdminPage";
 
-it('render Admin Page ', () => {
+it('render Admin Page', () => {
   const adminPage =  shallow(<AdminPage />);
   expect(adminPage).toMatchSnapshot();
 });
