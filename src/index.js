a11dbc64de10533f01f2f61c1e25c4ce50f34d81
Tim Christovich <christ10@my.erau.edu>
2019-05-14 20:17:07 -0400

    Remove App.js and consolidate behavior into index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
 import React from 'react';
+import {fab} from "@fortawesome/free-brands-svg-icons";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fas} from "@fortawesome/free-solid-svg-icons";
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
+import {Route, Switch} from 'react-router';
+import AppContainer from './App/AppContainer';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
+import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 
-import App from './App/App';
-import registerServiceWorker from './registerServiceWorker';
-import { loadResources } from './action/resourceDataAction';
+library.add(fab, fas);
+
+export const getRoutes = (store) => {        
+    return (
+        <div>
+            <Switch>
+                <Route path = '/' exact component={AppContainer} /> 
+                <Route path = '/admin' exact component={AppContainer} />
+                <Route 
+                    path = '/:resource/' 
+                    component={AppContainer} 
+                    dispatch={store.dispatch}
+                /> 
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    );
+};
 
 const store = configureStore();
-// Dispatch actions to load initial state.
-store.dispatch(loadResources());
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            { getRoutes(store) }
         </BrowserRouter>
     </Provider>,
     document.getElementById('root'));
