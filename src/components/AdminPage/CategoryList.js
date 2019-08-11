a04c704021dfd7dc1ffc5feda0000794afb43df2
FMA126 <mhblanks@gmail.com>
2019-08-11 15:58:47 -0400

    Include proptypes and fix export issue
    1. updated proptypes
    2. fixed styling using the linter

--- src/components/AdminPage/CategoryList.js
+++ src/components/AdminPage/CategoryList.js
@@ -1,102 +1,118 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as resourceAction from '../../action/resourceDataAction';
-import { ListGroup, ListGroupItem, Label, Button } from 'reactstrap';
-import _ from 'lodash'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { ListGroup, ListGroupItem, Label, Button } from "reactstrap";
+import _ from "lodash";
+import * as resourceAction from "../../action/resourceDataAction";
 
-export class CategoryList extends Component {
-  constructor (props) {
+class CategoryList extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      selectedCategory: []
+      selectedCategory: [],
     };
   }
 
-  componentDidUpdate () {
+  componentDidUpdate() {
     const { selectedCategory } = this.state;
-    const { resource } = this.props
-    const filteredResource = []
+    const { resource } = this.props;
+    const filteredResource = [];
+
     if (selectedCategory.length === 0) {
-      this.props.actions.filterByCategories(resource)
+      this.props.actions.filterByCategories(resource);
     } else {
       resource.forEach(res => {
-        const isMatch = selectedCategory.some(cat => res.categories === cat)
+        const isMatch = selectedCategory.some(cat => res.categories === cat);
+
         if (isMatch) {
-          filteredResource.push(res)
+          filteredResource.push(res);
         }
-      })
-      this.props.actions.filterByCategories(filteredResource)
+      });
+      this.props.actions.filterByCategories(filteredResource);
     }
-}
+  }
 
   handleClick = async event => {
-    event.persist()
-    const isContains = event.target.classList.contains("list-group-item-success")
-    const selectedCategoryLength = this.state.selectedCategory.length
+    event.persist();
+    const isContains = event.target.classList.contains(
+      "list-group-item-success"
+    );
+
+    const selectedCategoryLength = this.state.selectedCategory.length;
 
     if (isContains && selectedCategoryLength === 1) {
       this.setState({
-        selectedCategory: []
-      })
+        selectedCategory: [],
+      });
     } else if (isContains) {
-      this.setState((prevState) => {
-        const selectedCategoryCopy = prevState.selectedCategory.slice()
-        _.remove(selectedCategoryCopy, (cat) => cat === event.target.innerHTML)
+      this.setState(prevState => {
+        const selectedCategoryCopy = prevState.selectedCategory.slice();
+        _.remove(selectedCategoryCopy, cat => cat === event.target.innerHTML);
 
         return {
-          selectedCategory: selectedCategoryCopy
-        }
+          selectedCategory: selectedCategoryCopy,
+        };
       });
     } else {
-      this.setState((prevState) => ({
-        selectedCategory: [...prevState.selectedCategory, event.target.innerHTML]
-        })
-      );
-    };
+      this.setState(prevState => ({
+        selectedCategory: [
+          ...prevState.selectedCategory,
+          event.target.innerHTML,
+        ],
+      }));
+    }
   };
 
   clearChecks = () => {
     this.setState({
-      selectedCategory: []
-    })
-};
+      selectedCategory: [],
+    });
+  };
 
-  render () {
-    const { selectedCategory } = this.state
-    const { categories } = this.props
-    categories.sort()
+  render() {
+    const { selectedCategory } = this.state;
+    const { categories } = this.props;
+    categories.sort();
     const categoryMenuItems = categories.map((curr, index) => (
-        <ListGroupItem
-          key = {index.toString()}
-          color = {_.indexOf(selectedCategory, curr) !== -1 ? "success" : ""}
-          onClick = {this.handleClick}
-        >
+      <ListGroupItem
+        key={index.toString()}
+        color={_.indexOf(selectedCategory, curr) !== -1 ? "success" : ""}
+        onClick={this.handleClick}
+      >
         {curr}
-        </ListGroupItem>
+      </ListGroupItem>
     ));
 
     return (
       <div>
         <Label>Filter by Category</Label>
-        <Button color="info" className="w-100" onClick={this.clearChecks}>Clear</Button>
-        <div style={{ height: '400px', overflow: 'auto', cursor: 'pointer' }}>
-          <ListGroup>
-            {categoryMenuItems}
-          </ListGroup>
+        <Button color="info" className="w-100" onClick={this.clearChecks}>
+          Clear
+        </Button>
+        <div style={{ height: "400px", overflow: "auto", cursor: "pointer" }}>
+          <ListGroup>{categoryMenuItems}</ListGroup>
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps (state, ownProps) {
+CategoryList.propTypes = {
+  resource: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+  filterByCategories: PropTypes.func.isRequired,
+  categories: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
   return {
     categories: state.categories,
     resource: state.resource,
   };
 }
-function mapDispatchToProps (dispatch) {
+
+function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(resourceAction, dispatch),
   };
@@ -104,5 +120,5 @@ function mapDispatchToProps (dispatch) {
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(CategoryList);
