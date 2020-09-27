7767d8c839dc9b4af260a653b941313c1db02f31
jaime-varela <varela.jaime01@gmail.com>
2020-09-26 23:00:56 -0400

    dependency updated bashing needed

--- src/components/MapPage/OrganizationMarker.js
+++ src/components/MapPage/OrganizationMarker.js
@@ -1,71 +1,20 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Marker, InfoWindow } from "react-google-maps";
+import React from "react";
+import { renderToString } from "react-dom/server";
 
-class OrganizationMarker extends Component {
-  state = {
-    open: this.props.open,
-  };
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.open !== this.props.open) {
-      this.updateOpen();
-    }
-  }
-
-  // scrollToElement  and handleClickOfInfoWindow is currently non-functional
-  updateOpen = () => {
-    this.setState({ open: this.props.open });
-  };
-
-  scrollToElement = () => {
-    this.setState({ open: true });
-  };
-
-  handleClickOfInfoWindow = e => {
-    const element = document.getElementById(e.currentTarget.id);
-    element.scrollIntoView();
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    return (
-      <Marker
-        optimize={false}
-        position={this.props.resource.coordinates}
-        onClick={this.scrollToElement}
-      >
-        {this.state.open && (
-          <InfoWindow onCloseClick={this.handleClose}>
-            <div>
-              {this.props.resource.groupedResources.map(resource => (
-                <div
-                  key={resource.id}
-                  id={resource.id}
-                  onClick={this.handleClickOfInfoWindow}
-                >
-                  <h3>{resource.name}</h3>
-                  <div>{resource.combinedaddress}</div>
-                  <div>{resource.tags}</div>
-                  <div>
-                    <a href={`tel:${resource.phone}`}>{resource.phone}</a>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </InfoWindow>
-        )}
-      </Marker>
-    );
-  }
-}
-
-OrganizationMarker.propTypes = {
-  open: PropTypes.bool.isRequired,
-  resource: PropTypes.object.isRequired,
+// returns the string needed for the google maps API infoWindow content
+export default groupedResource => {
+  return renderToString(
+    <div>
+      {groupedResource.groupedResources.map(resource => (
+        <div key={resource.id} id={`infoWindow${resource.id}`}>
+          <h3>{resource.name}</h3>
+          <div>{resource.combinedaddress}</div>
+          <div>{resource.tags}</div>
+          <div>
+            <a href={`tel:${resource.phone}`}>{resource.phone}</a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
-
-export default OrganizationMarker;
